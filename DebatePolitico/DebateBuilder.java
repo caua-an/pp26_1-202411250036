5d4c05e0d52ed4031ade17f1fc2127abcccc29e5
Caua <cauapxa@gmail.com>
2026-05-30 21:45:51 -0300

    feat: Vincular o eleitor(clonado) ao Candidato

--- DebatePolitico/DebateBuilder.java
+++ DebatePolitico/DebateBuilder.java
@@ -5,12 +5,34 @@ public class DebateBuilder {
     private List<Candidato> candidatosList;
     private List<Microfone> microfonesList;
     private Eleitor eleitor_modelo = new Eleitor("modelo");
+    private Fachada fachada = Fachada.getInstance();
+
     private int cont_mic = 0;
 
     public DebateBuilder addCandidato(int id, String nome){
         Microfone mic = new Microfone(cont_mic++);
         microfonesList.add(mic);
         candidatosList.add(new Candidato(id, nome, mic));
+        return this;
+    }
+    public DebateBuilder addEleitor(String nome, int id_cand){
+        boolean encontrado = false;
+        // vincular o eleitor ao seu candidato
+        for(Candidato c : candidatosList){
+            if(c.getId() == id_cand){
+                // usa o prototipo para clonar
+                Eleitor e = eleitor_modelo.clonar_prot();
+                e.setNome(nome);
+                fachada.vincularEleitor(e, c);
+                encontrado = true;
+                break;
+            }
+        }
+        // caso nao encontre o candidato com id_cand
+        if(!encontrado){
+            throw new RuntimeException("Candidato com id "+ id_cand +" não encontrado");
+        }
+
         return this;
     }
 
